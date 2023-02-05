ecd9062599508bb50265b14226e675636f1740fa
simran bhandari <simranbhandari036@gmail.com>
2023-02-05 10:55:42 +0530

    modified task

--- index.js
+++ index.js
@@ -12,21 +12,33 @@
 //console.log(document.images);
 
 //getElementById
-let headerTitle = document.getElementById('header-title');
-console.log(headerTitle);
-headerTitle.textContent = 'Hello';
+//let headerTitle = document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.textContent = 'Hello';
 //headerTitle.innerText = 'Goodbye';
-headerTitle.style.borderBottom = 'solid 4px #000';
+//headerTitle.style.borderBottom = 'solid 4px #000';
 
 //getElementsByClassname
-let items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello 2';
-items[1].style.fontWeight = 'bold';
+//let items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1]);
+//items[1].textContent = 'Hello 2';
+//items[1].style.fontWeight = 'bold';
 //items[1].style.backgroundColor = 'green';
-for(let i=0; i<items.length;i++){
-    items[i].style.backgroundColor = '#f4f4f4';
-    items[i].style.fontWeight = 'bold';
-    items[i].style.color = 'green';
+//for(let i=0; i<items.length;i++){
+ //   items[i].style.backgroundColor = '#f4f4f4';
+ //   items[i].style.fontWeight = 'bold';
+ //   items[i].style.color = 'green';
+//}
+
+//getElementsByTagName
+let li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+
+li[2].style.backgroundColor = 'green';
+
+for(let i=0;i<li.length;i++){
+    li[i].style.fontWeight = 'bold';
 }
