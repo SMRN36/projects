b6bcda2e4cd595fc7e49294ce71ef3f9269d2f1c
simran bhandari <simranbhandari036@gmail.com>
2023-02-06 18:19:43 +0530

    storage

---
+++ Storage/index.js
@@ -0,0 +1,18 @@
+function storeFormData() {
+    var name = document.querySelector("#name").value;
+    var email = document.querySelector("#email").value;
+    var password = document.querySelector("#password").value;
+    var confirmPassword = document.querySelector("#confirm-password").value;
+    
+    var formData = {
+      name: name,
+      email: email,
+      password: password
+    };
+    
+    localStorage.setItem("formData", JSON.stringify(formData));
+    console.log("Form data has been stored in local storage.");
+  }
+  
+
+  
