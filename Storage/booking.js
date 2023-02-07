8fed98fd555026707e6f74ec1b1aeb323574aae1
simran bhandari <simranbhandari036@gmail.com>
2023-02-07 12:01:05 +0530

    added delete functionality

--- Storage/booking.js
+++ Storage/booking.js
@@ -1,4 +1,4 @@
-// JavaScript code for storing data in local storage
+
 const form = document.querySelector('form');
 const display = document.querySelector('#display');
 
@@ -8,7 +8,7 @@ form.addEventListener('submit', (e) => {
   const email = document.querySelector('#email').value;
   const phone = document.querySelector('#phone').value;
 
-  const userData = { name, email, phone };
+  const userData = { name, email, phone, id: Date.now() };
   let storedData = JSON.parse(localStorage.getItem('userData')) || [];
   storedData.push(userData);
   localStorage.setItem('userData', JSON.stringify(storedData));
@@ -24,9 +24,24 @@ function displayData() {
     data += `<p>Name: ${user.name}</p>
              <p>Email: ${user.email}</p>
              <p>Phone: ${user.phone}</p>
+             <button class="delete-btn" data-id="${user.id}">Delete</button>
              <hr>`;
   });
   display.innerHTML = data;
+  addDeleteEventListeners();
+}
+
+function addDeleteEventListeners() {
+  const deleteBtns = document.querySelectorAll('.delete-btn');
+  deleteBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      const id = e.target.getAttribute('data-id');
+      const storedData = JSON.parse(localStorage.getItem('userData')) || [];
+      const updatedData = storedData.filter((user) => user.id != id);
+      localStorage.setItem('userData', JSON.stringify(updatedData));
+      displayData();
+    });
+  });
 }
 
 // Call displayData function on page load
