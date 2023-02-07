b3d6dcb97cb7c8aac07a178f8a7cd6e51d9869bf
simran bhandari <simranbhandari036@gmail.com>
2023-02-07 12:16:00 +0530

    added edit functionality

--- Storage/booking.js
+++ Storage/booking.js
@@ -1,12 +1,14 @@
-
 const form = document.querySelector('form');
 const display = document.querySelector('#display');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const phoneInput = document.querySelector('#phone');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const name = document.querySelector('#name').value;
-  const email = document.querySelector('#email').value;
-  const phone = document.querySelector('#phone').value;
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const phone = phoneInput.value;
 
   const userData = { name, email, phone, id: Date.now() };
   let storedData = JSON.parse(localStorage.getItem('userData')) || [];
@@ -14,6 +16,7 @@ form.addEventListener('submit', (e) => {
   localStorage.setItem('userData', JSON.stringify(storedData));
 
   displayData();
+  clearInputs();
 });
 
 // function to display data in local storage
@@ -24,11 +27,13 @@ function displayData() {
     data += `<p>Name: ${user.name}</p>
              <p>Email: ${user.email}</p>
              <p>Phone: ${user.phone}</p>
+             <button class="edit-btn" data-id="${user.id}">Edit</button>
              <button class="delete-btn" data-id="${user.id}">Delete</button>
              <hr>`;
   });
   display.innerHTML = data;
   addDeleteEventListeners();
+  addEditEventListeners();
 }
 
 function addDeleteEventListeners() {
@@ -42,6 +47,29 @@ function addDeleteEventListeners() {
       displayData();
     });
   });
+}
+
+function addEditEventListeners() {
+  const editBtns = document.querySelectorAll('.edit-btn');
+  editBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      const id = e.target.getAttribute('data-id');
+      const storedData = JSON.parse(localStorage.getItem('userData')) || [];
+      const user = storedData.find((user) => user.id == id);
+      const updatedData = storedData.filter((user) => user.id != id);
+      localStorage.setItem('userData', JSON.stringify(updatedData));
+      nameInput.value = user.name;
+      emailInput.value = user.email;
+      phoneInput.value = user.phone;
+      displayData();
+    });
+  });
+}
+
+function clearInputs() {
+  nameInput.value = '';
+  emailInput.value = '';
+  phoneInput.value = '';
 }
 
 // Call displayData function on page load
