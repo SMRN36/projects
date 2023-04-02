df7978e751c298383272265708a75e42f3b10830
simran bhandari <simranbhandari036@gmail.com>
2023-04-02 13:00:55 +0530

    server created

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.end('Hey there, I am Simran!');
+    console.log('My name was requested.');
+});
+
+server.listen(4000);
